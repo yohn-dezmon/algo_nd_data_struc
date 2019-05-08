4c38ec3a058845243b5619207b99b5c2fed6d8b0
John Desmond <38733157+yohn-dezmon@users.noreply.github.com>
2019-05-08 12:55:57 -0500

    Update imp_folding_method.py

--- chapter_5_psads/imp_folding_method.py
+++ chapter_5_psads/imp_folding_method.py
@@ -1,28 +1,39 @@
 import random
 
+# My implementation of a Hashtable class from scratch. 
+# this implementation utilizes the folding method of calculating hash values 
+# in order to minimize collisions and to evenly divide the numbers given 
 
 class Hashtable(object):
 
     def __init__(self, size):
+        # we utilize a list data structure to contain the hashtable
         self.initial_list = []
+        # size of the hashtable, should be a prime to guarantee all spots get checked
         self.size = size
 
     def create_table(self):
+        """ The hashtable is initialized with each slot equal to None for further insertion/comparison """
         for i in range(self.size):
             self.initial_list.append(None)
         return self.initial_list
 
     def fold_method_hash_func(self, elementy):
+        """ This function computes the hash function for each value provided as the 'elementy' parameter """
+        # First I convert the number into a string, utilizing the fact that each string in python is a list of individual characters 
         nstr = str(elementy)
+        # here I use a list comprehension to convert each string into an integer, now we have a list of integers
         nint = [int(n) for n in nstr]
 
-        # I want to make a for loop that only accesses the even and/or odd values
+        # enumerate allows us to access the index and value at that index for each element of the list
         for indx, val in enumerate(nint):
             plus1 = indx + 1
             if plus1 in range(len(nint)):
+                # this part of the function takes the nth element and concatenates it to the n + 1 element
+                # e.g. [1,2] >> '1' + '2' >> ['12'] 
                 nint[indx] = str(nint[indx]) + str(nint[plus1])
                 del nint[plus1]
-
+        # convert the double digit strings back to integers, we now have a list of double digit integers 
         nint = [int(i) for i in nint]
 
         # now that we have the two digit pairs, we can sum them
@@ -32,11 +43,12 @@ class Hashtable(object):
 
 
     def insert_into_table(self, list1):
-        """ list1 can be a list or a single item """
+        """ This inserts a list containing multiple values into a hashtable """
         for i in list1:
             hash_val = self.fold_method_hash_func(i)
 
-            # here I am inserting the values into the hashtable while using collision control
+            # here I am inserting the values into the hashtable while utilizing collision control
+            # currently, the slots will be overwritten if there are more elements inserted than there are spaces in the hashtable
             while self.initial_list[hash_val] != None:
                 hash_val = (hash_val + 3) % self.size
 
@@ -46,7 +58,10 @@ class Hashtable(object):
         return self.initial_list
 
     def search_table(self, numbrr):
+        """ search table for a given value, numbrr """
         hash_value = self.fold_method_hash_func(numbrr)
+        
+        # the loggin_var ensures that all slots in the hashtable are checked
         logging_var = 0
 
         while ((numbrr != self.initial_list[hash_value]) and (logging_var <= self.size)):
@@ -74,4 +89,4 @@ h = Hashtable(11)
 h.create_table()
 h.insert_into_table(my_randoms)
 print(h.initial_list)
-h.search_table(22)
+h.search_table(38587)
